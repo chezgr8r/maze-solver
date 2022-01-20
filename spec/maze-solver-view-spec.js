6217b30bda041d6459ceb51c62fc5b1a11b2e3a8
Austin Funk <austintatiousfunk@gmail.com>
2022-01-19 22:47:39 -0500

    Initialize maze-solver project

---
+++ spec/maze-solver-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import MazeSolverView from '../lib/maze-solver-view';
+
+describe('MazeSolverView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
